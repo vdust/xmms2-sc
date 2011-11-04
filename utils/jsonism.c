d78c866b198ae40e67b838e69c93a67bac825457
Raphaël Bois <virtualdust@gmail.com>
2011-11-04 15:07:11 +0100

    OTHER: Copy utils from xmms2's tests.

---
+++ utils/jsonism.c
@@ -0,0 +1,103 @@
+/*  XMMS2 - X Music Multiplexer System
+ *  Copyright (C) 2003-2011 XMMS2 Team
+ *
+ *  PLUGINS ARE NOT CONSIDERED TO BE DERIVED WORK !!!
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "xmmsc/xmmsv.h"
+#include "jsonism.h"
+#include "utils/json.h"
+
+static xmmsv_t *
+create_structure (int stack_offset, int is_object)
+{
+	if (is_object) {
+		return xmmsv_new_dict ();
+	} else {
+		return xmmsv_new_list ();
+	}
+}
+
+static xmmsv_t *
+create_data (int type, const char *data, uint32_t len)
+{
+	switch (type) {
+		case JSON_STRING:
+			return xmmsv_new_string (data);
+		case JSON_INT:
+			return xmmsv_new_int (atoi(data));
+		case JSON_FLOAT:
+			return xmmsv_new_error ("Float type not supported");
+		case JSON_NULL:
+			return xmmsv_new_none ();
+		case JSON_TRUE:
+			return xmmsv_new_int (1);
+		case JSON_FALSE:
+			return xmmsv_new_int (0);
+		default:
+			return xmmsv_new_error ("Unknown data type.");
+	}
+}
+
+static int
+append (xmmsv_t *obj, const char *key, uint32_t key_len, xmmsv_t *value)
+{
+	if (xmmsv_is_type (obj, XMMSV_TYPE_LIST)) {
+		xmmsv_list_append (obj, value);
+	} else if (xmmsv_is_type (obj, XMMSV_TYPE_DICT) && key) {
+		xmmsv_dict_set (obj, key, value);
+	} else {
+		/* Should never be reached */
+		assert (0);
+	}
+	xmmsv_unref (value);
+	return 0;
+}
+
+xmmsv_t *
+xmmsv_from_json (const char *spec)
+{
+	json_config conf = {
+		0, /* buffer_initial_size (0=default) */
+		0, /* max_nesting (0=no limit) */
+		0, /* max_data (0=no limit) */
+		1, /* allow_c_comments */
+		0, /* allow_yaml_comments */
+		NULL, /* user_calloc */
+		NULL /* user_realloc */
+	};
+	json_parser_dom dom;
+	json_parser parser;
+	xmmsv_t *value;
+
+	json_parser_dom_init (&dom,
+						  (json_parser_dom_create_structure) create_structure,
+						  (json_parser_dom_create_data) create_data,
+						  (json_parser_dom_append) append);
+	json_parser_init (&parser, &conf, json_parser_dom_callback, &dom);
+
+	json_parser_string (&parser, spec, strlen (spec), NULL);
+	assert (dom.root_structure != NULL);
+	assert (dom.stack_offset == 0);
+
+	value = (xmmsv_t *) dom.root_structure;
+
+	json_parser_dom_free (&dom);
+	json_parser_free (&parser);
+
+	return value;
+}
---
+++ utils/value_utils.c
@@ -0,0 +1,266 @@
+/*  XMMS2 - X Music Multiplexer System
+ *  Copyright (C) 2003-2011 XMMS2 Team
+ *
+ *  PLUGINS ARE NOT CONSIDERED TO BE DERIVED WORK !!!
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ */
+
+#include <stdlib.h> /* free() */
+#include <stdio.h>
+#include <string.h>
+
+#include "value_utils.h"
+#include "coll_utils.h"
+
+static int _xmmsv_compare (xmmsv_t *a, xmmsv_t *b, int ordered);
+
+int
+xmmsv_compare (xmmsv_t *a, xmmsv_t *b)
+{
+	return _xmmsv_compare (a, b, 1);
+}
+
+int
+xmmsv_compare_unordered (xmmsv_t *a, xmmsv_t *b)
+{
+	return _xmmsv_compare (a, b, 0);
+}
+
+static int
+_xmmsv_compare (xmmsv_t *a, xmmsv_t *b, int ordered)
+{
+	int type;
+
+	type = xmmsv_get_type (a);
+	if (type != xmmsv_get_type (b))
+		return 0;
+
+	switch (type) {
+	case XMMSV_TYPE_STRING: {
+		const char *sa, *sb;
+
+		if (!xmmsv_get_string (a, &sa) || !xmmsv_get_string (b, &sb))
+			return 0;
+
+		if (strcmp (sa, sb) != 0)
+			return 0;
+
+		break;
+	}
+	case XMMSV_TYPE_INT32: {
+		int ia, ib;
+
+		if (!xmmsv_get_int (a, &ia) || !xmmsv_get_int (b, &ib))
+			return 0;
+
+		if (ia != ib)
+			return 0;
+
+		break;
+	}
+	case XMMSV_TYPE_DICT: {
+		xmmsv_dict_iter_t *it;
+
+		if (xmmsv_dict_get_size (a) != xmmsv_dict_get_size (b))
+			return 0;
+
+		xmmsv_get_dict_iter (a, &it);
+		while (xmmsv_dict_iter_valid (it)) {
+			xmmsv_t *ea, *eb;
+			const char *key;
+
+			xmmsv_dict_iter_pair (it, &key, &ea);
+
+			if (!xmmsv_dict_get (b, key, &eb))
+				return 0;
+
+			if (!_xmmsv_compare (ea, eb, ordered))
+				return 0;
+
+			xmmsv_dict_iter_next (it);
+		}
+		xmmsv_dict_iter_explicit_destroy (it);
+
+		break;
+	}
+	case XMMSV_TYPE_LIST: {
+		int size, i, j;
+		int match = 1;
+		char *matched = NULL;
+
+		size = xmmsv_list_get_size (a);
+		if (size != xmmsv_list_get_size (b))
+			return 0;
+
+		if (size && !ordered) {
+			matched = (char *) calloc (size, sizeof (char));
+		}
+
+		for (i = 0; i < size; i++) {
+			xmmsv_t *ea, *eb;
+
+			if (!xmmsv_list_get (a, i, &ea) || !xmmsv_list_get (b, i, &eb)) {
+				match = 0;
+				break;
+			}
+
+			if (ordered) {
+				match = _xmmsv_compare (ea, eb, ordered);
+				if (!match)
+					break;
+			} else {
+				match = 0;
+				for (j = 0; j < size; j++) {
+					if (matched[j])
+						continue;
+					if (!xmmsv_list_get (b, j, &eb))
+						break;
+					if (_xmmsv_compare (ea, eb, ordered)) {
+						matched[j] = 1;
+						match = 1;
+						break;
+					}
+				}
+				if (!match)
+					return 0;
+			}
+		}
+
+		if (matched)
+			free (matched);
+
+		return match;
+	}
+	case XMMSV_TYPE_COLL: {
+		xmmsv_coll_t *ca, *cb;
+
+		xmmsv_get_coll (a, &ca);
+		xmmsv_get_coll (b, &cb);
+
+		return xmmsv_coll_compare (ca, cb);
+	}
+	default: {
+		return 0;
+	}
+	}
+
+	return 1;
+}
+
+static void
+_xmms_dump_indent (int indent)
+{
+	int i;
+	for (i = 0; i < indent; i++)
+		printf ("  ");
+}
+
+static void
+_xmms_dump (xmmsv_t *value, int indent)
+{
+	int type;
+
+	if (value == NULL) {
+		printf ("xmmsv_t is NULL!\n");
+		return;
+	}
+
+	if (xmmsv_is_error (value)) {
+		const char *message;
+		xmmsv_get_error (value, &message);
+		printf ("error: %s\n", message);
+		return;
+	}
+
+	type = xmmsv_get_type (value);
+
+	switch (type) {
+	case XMMSV_TYPE_INT32: {
+		int val;
+		xmmsv_get_int (value, &val);
+		printf ("%d", val);
+		break;
+	}
+	case XMMSV_TYPE_STRING: {
+		const char *val;
+		xmmsv_get_string (value, &val);
+		printf ("'%s'", val);
+		break;
+	}
+	case XMMSV_TYPE_LIST: {
+		xmmsv_list_iter_t *iter;
+		xmmsv_get_list_iter (value, &iter);
+
+		printf ("[");
+		while (xmmsv_list_iter_valid (iter)) {
+			xmmsv_t *item;
+
+			xmmsv_list_iter_entry (iter, &item);
+
+			_xmms_dump (item, indent + 1);
+
+			xmmsv_list_iter_next (iter);
+			if (xmmsv_list_iter_valid (iter))
+				printf (", ");
+		}
+		printf ("]");
+		break;
+	}
+	case XMMSV_TYPE_DICT: {
+		xmmsv_dict_iter_t *iter;
+
+		xmmsv_get_dict_iter (value, &iter);
+
+		printf ("{\n");
+		while (xmmsv_dict_iter_valid (iter)) {
+			const char *key;
+			xmmsv_t *item;
+
+			xmmsv_dict_iter_pair (iter, &key, &item);
+
+			_xmms_dump_indent (indent + 1);
+			printf ("'%s': ", key);
+			_xmms_dump (item, indent + 1);
+
+			xmmsv_dict_iter_next (iter);
+			if (xmmsv_dict_iter_valid (iter))
+				printf (", ");
+			printf ("\n");
+		}
+		_xmms_dump_indent (indent);
+		printf ("}");
+
+		break;
+	}
+	case XMMSV_TYPE_COLL: {
+		xmmsv_coll_t *coll;
+		xmmsv_get_coll (value, &coll);
+		xmmsv_coll_dump_indented (coll, indent);
+		break;
+	}
+	default:
+		printf ("invalid type: %d\n", type);
+	}
+}
+
+void
+xmmsv_dump_indented (xmmsv_t *value, int indent)
+{
+	_xmms_dump (value, indent);
+}
+
+void
+xmmsv_dump (xmmsv_t *value)
+{
+	_xmms_dump (value, 0);
+	printf ("\n");
+}
